01f2e03742b8978b72d730b218d65cfc46f72977
Alexx_lml <inkisidor_117@hotmail.com>
2022-09-02 15:20:58 -0500

    iFrame dataStudio en Dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -8,7 +8,13 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                <x-jet-welcome />
+                <div class="w-full p-4 mt-20 mb-20">
+                    <div class="flex justify-center w-full ">
+                        <iframe width="800" height="500"
+                            src="https://datastudio.google.com/embed/reporting/c2a83db4-22e1-4dfb-92dd-7031c85ab604/page/LuBV" frameborder="0"
+                            style="border:0" allowfullscreen></iframe>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
