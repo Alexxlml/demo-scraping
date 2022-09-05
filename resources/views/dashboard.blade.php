1beb781dd1d0381ce27c2cf09388457fa511f64b
Alexx_lml <inkisidor_117@hotmail.com>
2022-09-05 10:10:26 -0500

    Iframe a pantalla completa

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,20 +1,14 @@
 <x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
-    </x-slot>
+    <div>
+        <div class="mx-auto">
+            <div style="height: 93vh;" class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                <div class="w-full p-4 mt-20 mb-20">
-                    <div class="flex justify-center w-full ">
-                        <iframe width="800" height="500"
-                            src="https://datastudio.google.com/embed/reporting/c2a83db4-22e1-4dfb-92dd-7031c85ab604/page/LuBV" frameborder="0"
-                            style="border:0" allowfullscreen></iframe>
-                    </div>
+                <div class="flex justify-center w-full h-full">
+                    <iframe style="width: 100%; height: 100%;"
+                        src="https://datastudio.google.com/embed/reporting/c2a83db4-22e1-4dfb-92dd-7031c85ab604/page/LuBV"
+                        frameborder="0" style="border:0" allowfullscreen></iframe>
                 </div>
+
             </div>
         </div>
     </div>
